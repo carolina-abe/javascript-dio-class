0c67c2f71cd7e7c161d7469e81dc5467bf27581f
Carolina Abe <carolina.abe.silva@gmail.com>
2024-04-08 21:52:02 -0300

    Adds fuelType to function calcFuelExpense

--- src/js/calcFuelExpense.js
+++ src/js/calcFuelExpense.js
@@ -1,10 +1,21 @@
-function calcFuelExpense (fuelPrice, kilometerPerLitre, kilometers) {
+function calcFuelExpense (fuelType, kilometerPerLitre, kilometers) {
+  switch(fuelType) {
+    case 'ethanol':
+      var fuelPrice = 3.19;
+      break;
+    case 'gasoline':
+      var fuelPrice = 5.17;
+      break;
+    default:
+      console.log('Fuel type invalid.');
+  }
+
   var litresConsumed = kilometers / kilometerPerLitre
   var totalSpent = litresConsumed * fuelPrice
-  
+
   console.log(totalSpent.toFixed(2))
 
   return totalSpent.toFixed(2)
 }
 
-calcFuelExpense(5.17, 12, 650)
+calcFuelExpense('ethanol', 12, 150)
